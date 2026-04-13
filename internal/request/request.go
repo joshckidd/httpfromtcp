25a5e2cf470a20eb096116345a979a95a82c92b7
joshckidd <josh@larameekidd.com>
2026-04-13 18:39:44 -0400

    incremental parser

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,14 +1,23 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"regexp"
 	"strings"
 )
 
+type State int
+
+const (
+	Initialized State = iota
+	Done
+)
+
 type Request struct {
 	RequestLine RequestLine
+	State       State
 }
 
 type RequestLine struct {
@@ -18,31 +27,76 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	req, err := io.ReadAll(reader)
-	if err != nil {
-		return &Request{}, err
+	req := Request{
+		State: Initialized,
 	}
-	return parseRequestLine(strings.Split(string(req), "\r\n")[0])
+	buf := make([]byte, 8)
+	bytesRead := 0
+
+	for req.State != Done {
+		n, err := reader.Read(buf[bytesRead:])
+		if err == io.EOF {
+			req.State = Done
+			break
+		}
+		if err != nil {
+			return &req, err
+		}
+		bytesRead += n
+
+		if bytesRead == len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf)
+			buf = newBuf
+		}
+
+		m, err := req.parse(buf[:bytesRead])
+		if err != nil {
+			return &req, err
+		}
+
+		if m != 0 {
+			newBuf := make([]byte, len(buf))
+			copy(newBuf, buf[bytesRead:])
+			buf = newBuf
+			bytesRead -= m
+		}
+	}
+
+	return &req, nil
 }
 
-func parseRequestLine(line string) (*Request, error) {
+func parseRequestLine(req []byte) (*Request, int, error) {
+	lines := bytes.Split(req, []byte("\r\n"))
+	if len(lines) == 1 {
+		return &Request{}, 0, nil
+	}
+
+	lineBytes := lines[0]
+	n := len(lineBytes) + 2
+	line := string(lineBytes)
+
 	parts := strings.Split(line, " ")
 
+	if len(parts) < 3 {
+		return &Request{}, n, errors.New("Invalid number of parts.")
+	}
+
 	if len(parts) > 3 {
-		return &Request{}, errors.New("Invalid number of parts.")
+		return &Request{}, n, errors.New("Invalid number of parts.")
 	}
 	r, _ := regexp.Compile("[^A-Z]+")
 	if r.MatchString(parts[0]) {
-		return &Request{}, errors.New("Method must be only capital alphabetic characters.")
+		return &Request{}, n, errors.New("Method must be only capital alphabetic characters.")
 	}
 	var version string
 	if strings.Contains(parts[2], "/") {
 		version = strings.Split(parts[2], "/")[1]
 		if version != "1.1" {
-			return &Request{}, errors.New("Invalid http version.")
+			return &Request{}, n, errors.New("Invalid http version.")
 		}
 	} else {
-		return &Request{}, errors.New("Invalid http version.")
+		return &Request{}, n, errors.New("Invalid http version.")
 	}
 
 	return &Request{
@@ -51,5 +105,15 @@ func parseRequestLine(line string) (*Request, error) {
 			RequestTarget: parts[1],
 			HttpVersion:   version,
 		},
-	}, nil
+	}, n, nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	req, n, err := parseRequestLine(data)
+
+	if n != 0 {
+		r.State = Done
+		r.RequestLine = req.RequestLine
+	}
+	return n, err
 }
