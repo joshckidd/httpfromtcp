d2c9ad403155aff4f84cba4841412bd2db6d8b01
joshckidd <josh@larameekidd.com>
2026-04-18 12:45:52 -0400

    setting up header parse

---
+++ internal/headers/headers.go
@@ -0,0 +1,41 @@
+package headers
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+)
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return make(Headers)
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	lines := bytes.Split(data, []byte("\r\n"))
+	if len(lines) == 1 {
+		return 0, false, nil
+	}
+
+	lineBytes := lines[0]
+	if len(lineBytes) == 0 {
+		return 2, true, nil
+	}
+
+	l := len(lineBytes) + 2
+	line := string(lineBytes)
+
+	parts := strings.SplitN(line, ":", 2)
+	if len(parts) != 2 {
+		return 0, false, errors.New("Header line does not contain :.")
+	}
+
+	if strings.TrimSpace(parts[0]) != parts[0] {
+		return 0, false, errors.New("There must be no whitespace in the field name.")
+	}
+
+	h[parts[0]] = strings.TrimSpace(parts[1])
+
+	return l, false, nil
+}
